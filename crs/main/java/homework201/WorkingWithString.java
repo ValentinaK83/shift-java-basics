35e0d51e9178054ad35c2bd6c014707f6013983b
ValentinaK83 <kulikova83@yandex.ru>
2026-05-22 19:59:31 +0700

    В домашнюю работу по задаче № 1 домашнего задания № 20 внесены изменения в соответствии с замечаниями

--- crs/main/java/homework201/WorkingWithString.java
+++ crs/main/java/homework201/WorkingWithString.java
@@ -5,16 +5,24 @@ import java.util.Scanner;
 public class WorkingWithString {
 
     static class TwoString {
-        private String originalString;
         private String modifiedString;
+        private String originalString;
+
+        public String getOriginalString() {
+            return originalString;
+        }
+
+        public String getModifiedString() {
+            return modifiedString;
+        }
 
         public TwoString(String originalString, String modifiedString) {
             this.originalString = originalString;
             this.modifiedString = modifiedString;
         }
 
-        public String toString() {
-            return "Исходная строка: " + originalString + "\nПолучившаяся строка: " + modifiedString;
+        public static void print(TwoString ts) {
+            System.out.println("Исходная строка: " + ts.getOriginalString() + "\nПолучившаяся строка: " + ts.getModifiedString());
         }
     }
 
@@ -31,17 +39,17 @@ public class WorkingWithString {
             System.out.println("Ошибка: необходимо ввести один символ (не пробел)");
             return;
         }
-        char targetSymbol = inputSymbol.charAt(0);
 
-        String cleanString = inputOriginalString.replaceAll("\\s+", " ");
+        String cleanString = inputOriginalString.replaceAll("\\s{2,}", " ");
 
-        String replacedString = cleanString.replace(String.valueOf(targetSymbol), " ");
 
-        String finalString = replacedString.replaceAll("\\s+", " ");
+        String replacedString = cleanString.replace(String.valueOf(inputSymbol), " ");
+
+        String finalString = replacedString.replaceAll("\\s{2,}", " ");
 
         TwoString result = new TwoString(inputOriginalString, finalString);
 
-        System.out.println(result);
+        TwoString.print(result);
 
         if (inputOriginalString.equals(finalString)) {
             System.out.println("После преобразования строка не изменилась");
